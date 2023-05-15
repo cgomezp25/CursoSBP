229e64c5e3021ff36c64451ce9b34599d3b5144d
CGOMEZ <cgomez@superbancos.gob.pa>
2023-05-15 13:36:35 -0500

    Create DataContext and Model Basic Projects.

---
+++ GITHUB/CursoSBP.Common/Services/IApiService.cs
@@ -0,0 +1,17 @@
+﻿using CursoSBP.Common.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoSBP.Common.Services
+{
+    public interface IApiService
+    {
+        Response GetApi(string url, string controller, string endpoint);
+        Response PostApi(string url, string controller, string endpoint);
+        Response PutApi(string url, string controller, string endpoint);
+        Response DeleteApi(string url, string controller, string endpoint);
+    }
+}
